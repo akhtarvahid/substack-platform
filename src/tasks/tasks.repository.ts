90e76679fb64aea8cdffa30e682b23ef1468d020
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-08-28 00:05:13 +0530

    logger added for debugging

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -4,9 +4,12 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task } from './task.entity';
 import { TaskStatus } from './task.status.enum';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
+  private logger = new Logger('TasksRepository', {timestamp: true}); // make it true to see load time
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -25,8 +28,13 @@ export class TasksRepository extends Repository<Task> {
       );
     }
 
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch(error) {
+      this.logger.error(`Failed to get tasks for user ${user.username}. Filter: ${JSON.stringify(filterDto)}`)
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
