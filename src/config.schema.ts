73a216cc76b36b08e3f6329e11f0339c2e8131c0
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-08-30 23:58:44 +0530

    docker compose working now

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -3,9 +3,9 @@ const Joi = require('joi'); // CommonJS
 export const configValidationSchema = Joi.object({
     STAGE: Joi.string().required(),
     DB_HOST: Joi.string().required(),
-    DB_PORT: Joi.string().required(),
-    DB_USERNAME: Joi.string().required(),
-    DB_PASSWORD: Joi.string().required(),
-    DB_DATABASE: Joi.string().required(),
+    POSTGRES_PORT: Joi.string().required(),
+    POSTGRES_USER: Joi.string().required(),
+    POSTGRES_PASSWORD: Joi.string().required(),
+    POSTGRES_DB: Joi.string().required(),
     JWT_SECRET: Joi.string().required()
   })
