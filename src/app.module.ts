69dd6130a39c4678c97dd57cb4756bd4501bbe32
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-27 23:39:03 +0530

    auth(user) module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from './user/user.module';
 import config from "./ormconfig";
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import config from "./ormconfig";
     }),
     TypeOrmModule.forRoot(config),
     TagModule,
+    UserModule,
   ],
 })
 export class AppModule {}
