9fa253cc8af399a15828f6f60d485273f697d901
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-27 20:45:28 +0530

    get all tags api created

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { TagService } from './tag.service';
+import { TagEntity } from './tag.entity';
 
-@Controller('tag')
+@Controller('tags')
 export class TagController {
-    @Get('/hello')
+    @Inject() tagService: TagService;
+    @Get('/health')
     hello(){
-        return 'hello'
+        return 'UP'
+    }
+
+    @Get()
+    async findAllTags(): Promise<{tags: string[]}> {
+        const tags = await this.tagService.findAll();
+        return {
+            tags: tags.map(tag => tag.tagName)
+        }
     }
 }
--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TagEntity } from './tag.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+    constructor(@InjectRepository(TagEntity) private readonly tagRepository: Repository<TagEntity>){}
+    async findAll(): Promise<TagEntity[]> {
+        return await this.tagRepository.find();
+    }
+}
