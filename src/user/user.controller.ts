0ef1c54156e5d39c8058e324bf821ee6a8790923
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-28 00:44:28 +0530

    dto with createUser endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
@@ -8,5 +9,10 @@ export class UserController {
      @Get("/health")
      health() {
         return this.userService.health();
+     }
+
+     @Post()
+     async createUser(@Body() createUserDto: CreateUserDto) {
+        return this.userService.create(createUserDto);
      }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Injectable()
 export class UserService {
     health() {
         return 'UP'
+    }
+
+    async create(createUserDto: CreateUserDto) {
+      return await createUserDto;
     }
 }
