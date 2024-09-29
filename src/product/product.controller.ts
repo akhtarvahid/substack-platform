492ae688388858a6562691da86a853e7c3d5c43e
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-29 14:03:58 +0530

    health api for product module

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+    @Get('/health')
+    health() {
+        return 'UP'
+    }
+}
