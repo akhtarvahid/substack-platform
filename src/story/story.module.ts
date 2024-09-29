9fda71db9c1ce644af776f32a2f4a24608e4368a
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-29 14:33:20 +0530

    endpoint without migration and saving in db

--- src/story/story.module.ts
+++ src/story/story.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { StoryController } from './story.controller';
 import { StoryService } from './story.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { StoryEntity } from './entities/story.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StoryEntity])],
   controllers: [StoryController],
   providers: [StoryService]
 })
