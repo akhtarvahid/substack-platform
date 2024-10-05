597c6abaadebbf72de6102757859fe98cb795d11
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-10-05 18:57:17 +0530

    [FIXED]-[error->Cannot read properties of null (reading 'constructor')]

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-if(!process.env.IS_TS_NODE) {
-  require('module-alias/register');
+if (!process.env.IS_TS_NODE) {
+  require("module-alias/register");
 }
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { TransformInterceptor } from './transform.interceptor';
-import { GlobalValidationPipe } from './shared/pipes/global-validation.pipe';
+
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { TransformInterceptor } from "./transform.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new GlobalValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
