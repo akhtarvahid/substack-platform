5de2cf551306d089ed9a0f248fb5d1215799a4b2
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-10-07 00:45:09 +0530

    cors enabled to make it working from other domain

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from "./app.module";
 import { TransformInterceptor } from "./transform.interceptor";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
