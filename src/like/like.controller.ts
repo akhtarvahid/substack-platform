63ebcc288b49810df8a3e0c81ed5191468894b14
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-10-12 20:06:14 +0530

    health endpoint

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 
 @Controller("like")
-export class LikeController {}
+export class LikeController {
+  @Get("/health")
+  heath() {
+    return "Like is UP";
+  }
+}
